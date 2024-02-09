f869242c9ac9d87b451671ad6555dff3be6c24dd
AndrewRaphael10 <andrewrgpires@gmail.com>
2024-02-09 13:39:24 -0300

    First_Program

--- PrimeiroPrograma em C/main.c
+++
@@ -1,5 +0,0 @@
-int main() {
-    
-    printf("Meu primeiro programa! \n");
-    return 0;
-}
---
+++ primeiro_programa_em-c/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+  printf("Meu Primeiro Programa!\n");
+  return 0;
+}
