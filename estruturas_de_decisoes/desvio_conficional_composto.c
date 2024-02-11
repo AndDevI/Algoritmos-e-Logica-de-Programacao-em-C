13556348a05c5df86ad2a90d4e1ac3018c4bddd1
AndrewRaphael10 <andrewrgpires@gmail.com>
2024-02-11 15:08:38 -0300

    compound_conditional

---
+++ estruturas_de_decisoes/desvio_conficional_composto.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+  int numero;
+
+  printf("Digite um numero: ");
+    scanf("%d", &numero);
+
+    if (numero > 0){  /*SE o numero digitado pelo usuario é maior que 0 */
+      printf("POSITIVO \n");
+    } else {
+      printf("NEGATIVO \n");
+    }
+    
+}
