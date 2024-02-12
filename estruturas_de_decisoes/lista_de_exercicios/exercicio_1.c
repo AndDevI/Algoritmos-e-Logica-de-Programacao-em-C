9e6840195a2a632717ea65f1bd2e009e03c6b36e
AndrewRaphael10 <andrewrgpires@gmail.com>
2024-02-12 11:55:02 -0300

    exercise_1

---
+++ estruturas_de_decisoes/lista_de_exercicios/exercicio_1.c
@@ -0,0 +1,22 @@
+//Escreva um programa que leia dois valores inteiros quaisquer, e escreva o maior deles. 
+
+int main() {
+    int valor1, valor2;
+
+    printf("Digite o primeiro valor: ");
+    scanf("%d", &valor1);
+
+    printf("Digite o segundo valor: ");
+    scanf("%d", &valor2);
+
+   
+    if (valor1 > valor2) {
+        printf("O maior valor é: %d\n", valor1);
+    } else if (valor2 > valor1) {
+        printf("O maior valor é: %d\n", valor2);
+    } else {
+        printf("Os valores são iguais.\n");
+    }
+
+    return 0;
+}
