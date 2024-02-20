49928cd76ef546e8c21939f2ba8464c3084894ae
Andrew <andrewrgpires@gmail.com>
2024-02-20 09:52:50 -0300

    repetition_FOR

---
+++ estruturas_de_repeticoes/repeticao_FOR.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+  int numero, resultado;
+  int i;
+
+  for (i = 0; i < 5; i++) {               /*Laço de repetição tem uma quantidade de repetições */
+    printf("Digite um numero:\n");
+      scanf("%d", &numero);
+  
+    resultado = numero * 3;
+  
+    printf("Resultado foi: %d \n", resultado);
+  }
+
+  return 0;
+}
